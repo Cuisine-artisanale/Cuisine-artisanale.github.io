74b9690019583e28cbc388434326454cf84f5c4e
Aymeric Sabatier <ssabatieraymeric@gmail.com>
2026-01-06 14:08:42 +0100

    [Fix] liste de course

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -28,6 +28,11 @@ export default function AccountSectionLayout({ children }: { children: ReactNode
 			icon: 'pi pi-heart'
 		},
 		{
+			href: '/account/a-faire',
+			label: 'À Faire',
+			icon: 'pi pi-bookmark'
+		},
+		{
 			href: '/account/liste-de-course',
 			label: 'Liste de Course',
 			icon: 'pi pi-shopping-cart'
